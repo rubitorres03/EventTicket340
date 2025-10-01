f8a69f664abb257324817043caa3c09bce26de7d
Rubi Torres <159514596+rubitorres03@users.noreply.github.com>
2025-10-01 23:57:43 +0000

    new

---
+++ VenueEvent.h
@@ -0,0 +1,6 @@
+#ifndef VENUE_EVENT_
+#define VENUE_EVENT_
+
+String venue;       //data fields
+String dateTime;
+int capacity;
---
+++ VirtualEvent.h
@@ -0,0 +1,5 @@
+#ifndef VIRTUAL_EVENT_
+#define VIRTUAL_EVENT_
+
+String streamLink;
+String audience;
