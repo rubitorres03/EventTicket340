235ef2e06e60dca66de386de9e24afcfb13112aa
Rubi Torres <159514596+rubitorres03@users.noreply.github.com>
2025-10-02 01:06:21 +0000

    more

--- VirtualEvent.h
+++ VirtualEvent.h
@@ -1,5 +1,19 @@
 #ifndef VIRTUAL_EVENT_
 #define VIRTUAL_EVENT_
 
-String streamLink;
-String audience;
+#include "Event.h"
+
+#include <iostream>
+#include <String>
+
+class VirtualEvent : public Event{
+    protected:
+        std::String streamLink;
+        std::String audience;
+
+public:
+    VirtualEvent();
+    VirtualEvent(const std::string& streamLink, const std::string& audience);
+    ~VirtualEvent();
+
+}
