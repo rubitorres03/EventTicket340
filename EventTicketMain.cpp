cd659766375e82de190b5389c2cab9f273887ae1
04diana <dspaisano@gmail.com>
2025-10-06 23:35:15 +0000

    case 3 updates

--- EventTicketMain.cpp
+++ EventTicketMain.cpp
@@ -20,6 +20,7 @@ using namespace std;
 void displayOrganizerMenu(Organizer& organizer){
 	int organizerChoice = 0;
 	do {
+		LinkedBag<Event*> organizerEvents;
 		cout << "\n Hi, "<< organizer.getUsername() <<", what would you like to do:\n"
 		<< "1. Display Profile\n"
 		<< "2. Modify Password\n"
@@ -52,13 +53,86 @@ void displayOrganizerMenu(Organizer& organizer){
 			}
 			case 3: {
 				// TO DO: ask organizer to choose event type, then ask them to input event details.
+				std::string choice;
+				Event* event = nullptr;
+				
+				cout << "Which event type are you making (virtual or venue):  ";
+				getline(cin,choice);
+				if(choice == "virtual"){
+				// 	create a Virtual event object
+					std::string name;
+					std::string description;
+					int rating;
+					std::string ratingStr;
+					int soldTicketsCount;
+					std::string ticketsStr;
+					std::string streamLink;
+					std::string audience;
+
+					std::cout<< "Enter name of Event:";
+					std::getline(std::cin,name);
+
+					cout<< "Enter description: ";
+					std::getline(std::cin,description);
+
+					std::cout<< "Enter rating:";
+					std::getline(std::sin,ratingStr);
+					rating = std::stoi(ratingStr);
+
+					std::cout<< "Enter number of sold Tickets: ";
+					std::getline(std::sin,ticketsStr);
+					soldTicketsCount = std::stoi(ticketsStr);
+
+					std::cout<< "Enter Stream Link: ";
+					std::getline(std::cin,streamLink);
+
+					std::cout<< "Enter audience type:  ";
+					std::getline(std::cin, audience);
+
+					event = new VirtualEvent(name,description,rating,soldTicketsCount,streamLink,audience);
+					
+				}else if(choice == "venue"){
+				// 	create a Venue even object
+					std::string name;
+					std::string description;
+					int rating;
+					int soldTicketsCount;
+					std::string venue;
+					std::string dateTime;
+					int capacity;
+
+					std::cout<< "Enter name of Event:";
+					std::cin >> name;
+
+					cout<< "Enter description: ";
+					std::getline(std::cin,description);
+
+					std::cout<< "Enter rating:";
+					std::cin >> rating;
+
+					std::cout<< "Enter number of sold Tickets: ";
+					std::cin >> soldTicketsCount;
+
+					cout<< "Enter venue name: ";
+					std::cin >> venue;
+
+					cout<< "Enter date and Time: ";
+					std::getline(std::cin,dateTime);
+
+					std::cout<<"Enter capacity for event: ";
+					std::cin >> capacity;
+				
+					event = new VenueEvent(name,description,rating,soldTicketsCount,venue,dateTime,capacity);
+					
+				}
+				organizerEvents.add(event);
 				// Create the event and add it to the organizer's events
 				break;
 			}
 			case 4:{
 				// TO DO: display all organizer's events
 				//        You may re-use code from class demo
-				// event.display()
+				
 				break;
 			}
 			case 5: {
@@ -109,26 +183,26 @@ int main(){
 	cout << "\n Welcome to EventTicket340:" << endl;
 	// TO DO: Ask the organizer to enter their information 
 	//        Instantiate a new Organizer object
-	string username;
-	string email;
-	string password;
-	string bio;
-	string profilePicture;
+	std::string username;
+	std::string email;
+	std::string password;
+	std::string bio;
+	std::string profilePicture;
 
-	cout << "Please enter a username: ";
-	getline(cin,username);
+	std::cout << "Please enter a username: ";
+	std::cin >> username;
 
-	cout << "Please enter an email: ";
-	getline(cin,email);
+	std::cout << "Please enter an email: ";
+	std::getline(std::cin,email);
 
-	cout << "Please enter a password ";
-	getline(cin,password);
+	std::cout << "Please enter a password ";
+	std::cin >> password;
 
-	cout << "Please enter a bio: ";
-	getline(cin,bio);
+	std::cout << "Please enter a bio: ";
+	atd::getline(std::cin,bio);
 
-	cout << "Please enter a profile picture ";
-	getline(cin,profilePicture);
+	std::cout << "Please enter a profile picture ";
+	std::cin >> profilePicture;
 
 	// call eventTicket340 createOrganizer function 
 	// replace /*...*/ with the right parameters
